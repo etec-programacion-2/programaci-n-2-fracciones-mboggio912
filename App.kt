72f4e905443c2bc01f62f7074875893635b195e6
mateo <mf.boggio@alumno.etec.um.edu.ar>
2025-07-30 20:52:49 -0300

    Etapa 4: Validaciones y métodos de utilidad completados

--- App.kt
+++ App.kt
@@ -4,9 +4,10 @@
 package org.example
 
 fun main() {
-    var denominador2 = 10
     var numerador2 = 20
-    var fraccion = Fraccion(10,1)
+    var denominador2 = 10
+
+    var fraccion = Fraccion(-10,1)
     println(fraccion.mostrar())
     println(fraccion.sumar(denominador2, numerador2))
     println(fraccion.restar(denominador2, numerador2))
--- Fraccion.kt
+++ Fraccion.kt
@@ -57,7 +57,7 @@ class Fraccion (_numerador: Int, _denominador: Int) {
             return "El denominador no puede ser cero"
         }
         else {
-            var numeradornew = (this.numerador * denominador - numerador * this.denominador)
+            var numeradornew = this.numerador * denominador - numerador * this.denominador
             var denominadornew = (this.denominador * denominador)
             var finalfuncion = simplifyFraction(numeradornew, denominadornew)
             return finalfuncion
@@ -84,8 +84,8 @@ class Fraccion (_numerador: Int, _denominador: Int) {
         }
 
         else {
-            var numeradornew = (this.numerador * denominador)
-            var denominadornew = (this.denominador * numerador)
+            var denominadornew = (this.numerador * denominador)
+            var numeradornew = (this.denominador * numerador)
             var finalfuncion = simplifyFraction(numeradornew, denominadornew)
             return finalfuncion
         }
@@ -94,4 +94,69 @@ class Fraccion (_numerador: Int, _denominador: Int) {
 
 
 
+
+    fun esMayor (numerador: Int, denominador: Int): String{
+        var frac1 =this.numerador / this.denominador
+        var frac2 = numerador / denominador
+        if (frac1 > frac2){
+            return "mayor"
+        }
+        else if (frac2 > frac1){
+            return "menor"
+        }
+        else return "igual"
+
+    }
+
+    fun aDecimal (): Float{
+        var decimal = (this.numerador / this.denominador).toFloat()
+        return decimal
+
+    }
+
+    companion object {
+        fun desdeDecimal(decimal: Double): Fraccion {
+            // Casos especiales
+            if (decimal == 0.0) return Fraccion(0, 1)
+            if (decimal.isNaN() || decimal.isInfinite()) {
+                throw IllegalArgumentException("No se puede convertir NaN o infinito a fracción")
+            }
+
+            // Determinar si es negativo
+            val esNegativo = decimal < 0
+            val decimalAbs = kotlin.math.abs(decimal)
+
+            // Convertir a string para analizar los decimales
+            val decimalStr = decimalAbs.toString()
+
+            // Separar parte entera y decimal
+            val partes = decimalStr.split(".")
+            val parteEntera = partes[0].toLong()
+
+            if (partes.size == 1 || partes[1] == "0") {
+                // Es un número entero
+                val resultado = if (esNegativo) -parteEntera.toInt() else parteEntera.toInt()
+                return Fraccion(resultado, 1)
+            }
+
+            val parteDecimal = partes[1]
+            val numDigitosDecimales = parteDecimal.length
+
+            // Crear fracción: decimal = parteEntera + parteDecimal/10^numDigitos
+            val denominadorBase = kotlin.math.pow(10.0, numDigitosDecimales.toDouble()).toLong()
+            val numeradorDecimal = parteDecimal.toLong()
+            val numeradorTotal = parteEntera * denominadorBase + numeradorDecimal
+
+            // Aplicar signo si es necesario
+            val numeradorFinal = if (esNegativo) -numeradorTotal.toInt() else numeradorTotal.toInt()
+
+            return Fraccion(numeradorFinal, denominadorBase.toInt())
+        }
+    }
+
+
+
+
+
 }
+
